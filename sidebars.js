0ede290af76f1d14d7ec25fabaa080e3cf9fe4d4
二狗 <810505339@qq.com>
2021-03-10 00:06:43 +0800

    bug

--- sidebars.js
+++ sidebars.js
@@ -7,9 +7,11 @@ module.exports = {
         Interview: [
             'interview/vue/data为什么是一个函数',
             'interview/vue/key的作用',
+            'interview/vue/响应式原理',
             'interview/js/防抖跟节流',
             'interview/html/圣杯布局',
-            'interview/html/深拷贝和浅拷贝',
+            'interview/js/深拷贝和浅拷贝',
+            'interview/js/promise',
         ],
     },
 };
